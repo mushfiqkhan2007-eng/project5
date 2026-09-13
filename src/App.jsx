b6e81fd2448eeaa625c5a13828536875988aad22
mushfiqkhan2007-eng <mushfiqkhan2007@gmail.com>
2026-09-13 23:10:24 +0600

    more

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import TechnologiesSection from './components/TechnologiesSection'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white text-slate-900">
+      <Navbar />
+      <Hero />
+      <TechnologiesSection />
+      <Footer />
+      <ToastContainer position="top-right" autoClose={2500} />
+    </div>
+  )
+}
